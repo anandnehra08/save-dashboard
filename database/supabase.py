f2abc063557d16bf9c1f5e99128cdbd279d9c35c
anandnehra08 <anandnehra8@gmail.com>
2026-08-12 23:07:09 +0530

    Update supabase.py

--- database/supabase.py
+++ database/supabase.py
@@ -2,15 +2,15 @@ import os
 import streamlit as st
 from supabase import create_client, Client
 
-@st.cache_resource
+@st.cache_resource(ttl=600)
 def init_supabase() -> Client:
-    supabase_url = st.secrets.get("SUPABASE_URL") or os.environ.get("SUPABASE_URL")
-    supabase_key = st.secrets.get("SUPABASE_KEY") or os.environ.get("SUPABASE_KEY")
+    url = st.secrets.get("SUPABASE_URL") or os.environ.get("SUPABASE_URL")
+    key = st.secrets.get("SUPABASE_KEY") or os.environ.get("SUPABASE_KEY")
     
-    if not supabase_url or not supabase_key:
-        st.error("Supabase credentials missing in secrets.toml!")
+    if not url or not key:
+        st.error("Supabase URL or Key is missing in Secrets!")
         return None
         
-    return create_client(supabase_url, supabase_key)
+    return create_client(url, key)
 
 supabase = init_supabase()
